c49e5a004820519c8f0960ba54412056fe9c9350
Hiro51 <h.tsujino0714@gmail.com>
2014-11-21 16:57:35 +0900

    add a table view

---
+++ Homepwner_tutorial/BNRItemsViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRItemsViewController.h
+//  Homepwner_tutorial
+//
+//  Created by TsujinoHiroyuki on 11/21/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRItemsViewController : UITableViewController
+
+@end
---
+++ Homepwner_tutorial/BNRItemsViewController.m
@@ -0,0 +1,23 @@
+//
+//  BNRItemsViewController.m
+//  Homepwner_tutorial
+//
+//  Created by TsujinoHiroyuki on 11/21/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRItemsViewController.h"
+
+@implementation BNRItemsViewController
+
+- (instancetype)init {
+    // Call the superclass's designated initializer
+    self = [super initWithStyle:UITableViewStylePlain];
+    return self;
+}
+
+- (instancetype)initWithStyle:(UITableViewStyle)style {
+    return [self init];
+}
+
+@end
