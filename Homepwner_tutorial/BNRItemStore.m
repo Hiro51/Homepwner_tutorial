07d95cb0e69f826d97176993e79d5e2f60a3284e
Hiro51 <h.tsujino0714@gmail.com>
2014-12-08 22:18:01 +0900

    add reordering function

--- Homepwner_tutorial/BNRItemStore.m
+++ Homepwner_tutorial/BNRItemStore.m
@@ -62,6 +62,12 @@
     }
     // Get pointer to object being moved so you can re-insert it
     BNRItem *item = self.privateItems[fromIndex];
+    
+    // Remove item from the array
+    [self.privateItems removeObjectAtIndex:fromIndex];
+    
+    // Insert item in array at new location
+    [self.privateItems insertObject:item atIndex:toIndex];
 }
 
 
